5535ba2d038b9585ff2e6a189818c509877275eb
Berend <berend@botje-air-m2.kpn>
2023-08-18 17:05:34 +0200

    test updates

--- tst/test_botje_base64__decode.c
+++ tst/test_botje_base64__decode.c
@@ -5,181 +5,195 @@ void test_botje_base64__decode_binary_test(void **state) {
 
     // init and default values
     int64_t buf_size = 1024;
-    int64_t ret_val;
+    int64_t num_chars;
     int64_t str_src_len;
     unsigned char *str_dst = malloc(buf_size);
-    unsigned char *str_src = malloc(buf_size);
 
     // the following tests are the opposites of the encode tests
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "+A==", 4);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 1);
-    assert_int_equal(str_dst[0], 248);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "+A==";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 1);
+        unsigned char str_dst_cmp[] = { 248 };
+        assert_memory_equal(str_dst, str_dst_cmp, num_chars);
+    }
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "/A==", 4);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 1);
-    assert_int_equal(str_dst[0], 252);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "/A==";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 1);
+        unsigned char str_dst_cmp[] = { 252 };
+        assert_memory_equal(str_dst, str_dst_cmp, num_chars);
+    }
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "+/A=", 4);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 2);
-    assert_int_equal(str_dst[0], 251);
-    assert_int_equal(str_dst[1], 240);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "+/A=";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 2);
+        unsigned char str_dst_cmp[] = { 251, 240 };
+        assert_memory_equal(str_dst, str_dst_cmp, num_chars);
+    }
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "+/+/", 4);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 3);
-    assert_int_equal(str_dst[0], 251);
-    assert_int_equal(str_dst[1], 255);
-    assert_int_equal(str_dst[2], 191);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "+/+/";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 3);
+        unsigned char str_dst_cmp[] = { 251, 255, 191 };
+        assert_memory_equal(str_dst, str_dst_cmp, num_chars);
+    }
 
     // clean up
     free(str_dst);
-    free(str_src);
 }
 
 void test_botje_base64__decode_text_test(void **state) {
 
     // init and default values
     int64_t buf_size = 1024;
-    int64_t ret_val;
+    int64_t num_chars;
     int64_t str_src_len;
     unsigned char *str_dst = malloc(buf_size);
-    unsigned char *str_src = malloc(buf_size);
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TQ==", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 1);
-    assert_memory_equal(str_dst, "M", 1);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TQ==";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 1);
+        assert_memory_equal(str_dst, "M", num_chars);
+    }
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWE=", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 2);
-    assert_memory_equal(str_dst, "Ma", 2);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWE=";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 2);
+        assert_memory_equal(str_dst, "Ma", num_chars);
+    }
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWFu", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 3);
-    assert_memory_equal(str_dst, "Man", 3);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWFu";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 3);
+        assert_memory_equal(str_dst, "Man", num_chars);
+    }
 
-    str_src_len = 8;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWFuTQ==", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "ManM", 4);
+    {
+        str_src_len = 8;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWFuTQ==";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "ManM", num_chars);
+    }
 
-    str_src_len = 8;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWFuTWE=", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 5);
-    assert_memory_equal(str_dst, "ManMa", 5);
+    {
+        str_src_len = 8;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWFuTWE=";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 5);
+        assert_memory_equal(str_dst, "ManMa", num_chars);
+    }
 
-    str_src_len = 8;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWFuWFk=", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 5);
-    assert_memory_equal(str_dst, "ManXY", 5);
+    {
+        str_src_len = 8;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWFuWFk=";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 5);
+        assert_memory_equal(str_dst, "ManXY", num_chars);
+    }
 
-    str_src_len = 8;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWFuTWFu", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 6);
-    assert_memory_equal(str_dst, "ManMan", 6);
+    {
+        str_src_len = 8;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWFuTWFu";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 6);
+        assert_memory_equal(str_dst, "ManMan", num_chars);
+    }
 
-    str_src_len = 36;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 27);
-    assert_memory_equal(str_dst, "Many hands make light work.", 27);
+    {
+        str_src_len = 36;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 27);
+        assert_memory_equal(str_dst, "Many hands make light work.", num_chars);
+    }
 
     // clean up
     free(str_dst);
-    free(str_src);
 }
 
 void test_botje_base64__decode_wrong_input_test(void **state) {
 
     // init and default values
     int64_t buf_size = 1024;
-    int64_t ret_val;
+    int64_t num_chars;
     int64_t str_src_len;
     unsigned char *str_dst = malloc(buf_size);
-    unsigned char *str_src = malloc(buf_size);
 
-    str_src_len = 0;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 0);
+    {
+        str_src_len = 0;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 0);
+    }
 
-    str_src_len = 1;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "T", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, -1);
+    {
+        str_src_len = 1;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "T";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, -1);
+    }
 
-    str_src_len = 2;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TQ", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, -1);
+    {
+        str_src_len = 2;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TQ";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, -1);
+    }
     
-    str_src_len = 3;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWE", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, -1);
+    {
+        str_src_len = 3;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWE";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, -1);
+    }
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    unsigned char str_src_4[4] = { 0, 1, 2, 3 };
-    ret_val = botje_base64__decode(str_dst, str_src_4, str_src_len);
-    assert_int_equal(ret_val, -2);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 0, 1, 2, 3 };
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, -2);
+    }
 
-    str_src_len = 5;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "TWFuT", str_src_len);
-    ret_val = botje_base64__decode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, -1);
+    {
+        str_src_len = 5;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "TWFuT";
+        num_chars = botje_base64__decode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, -1);
+    }
 
     // clean up
     free(str_dst);
-    free(str_src);
 }
--- tst/test_botje_base64__encode.c
+++ tst/test_botje_base64__encode.c
@@ -5,61 +5,75 @@ void test_botje_base64__encode_binary_test(void **state) {
 
     // init and default values
     int64_t buf_size = 1024;
-    int64_t ret_val;
+    int64_t num_chars;
     int64_t str_src_len;
     unsigned char *str_dst = malloc(buf_size);
 
-    str_src_len = 1;
-    memset(str_dst, 0, buf_size);
-    unsigned char str_src_0[] = { 248 };
-    ret_val = botje_base64__encode(str_dst, str_src_0, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "+A==", 4);
+    {
+        str_src_len = 1;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 248 };
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "+A==", num_chars);
+    }
 
-    str_src_len = 1;
-    memset(str_dst, 0, buf_size);
-    unsigned char str_src_1[] = { 252 };
-    ret_val = botje_base64__encode(str_dst, str_src_1, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "/A==", 4);
+    {
+        str_src_len = 1;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 252 };
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "/A==", num_chars);
+    }
 
-    str_src_len = 2;
-    memset(str_dst, 0, buf_size);
-    unsigned char str_src_2[] = { 251, 240 };
-    ret_val = botje_base64__encode(str_dst, str_src_2, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "+/A=", 4);
+    {
+        str_src_len = 2;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 251, 240 };
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "+/A=", num_chars);
+    }
 
-    str_src_len = 3;
-    memset(str_dst, 0, buf_size);
-    unsigned char str_src_3[] = { 251, 255, 191 };
-    ret_val = botje_base64__encode(str_dst, str_src_3, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "+/+/", 4);
+    {
+        str_src_len = 3;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 251, 255, 191 };
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "+/+/", num_chars);
+    }
 
-    // // example from rfc4648
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    unsigned char str_src_4[] = { 0x14, 0xfb, 0x9c, 0x03 };
-    ret_val = botje_base64__encode(str_dst, str_src_4, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "FPucAw==", 8);
+    {
+        // example from rfc4648
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 0x14, 0xfb, 0x9c, 0x03 };
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "FPucAw==", num_chars);
+    }
 
-    // // example from rfc4648
-    str_src_len = 5;
-    memset(str_dst, 0, buf_size);
-    unsigned char str_src_5[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9 };
-    ret_val = botje_base64__encode(str_dst, str_src_5, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "FPucA9k=", 8);
+    {
+        // example from rfc4648
+        str_src_len = 5;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9 };
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "FPucA9k=", num_chars);
+    }
 
-    // // example from rfc4648
-    str_src_len = 6;
-    memset(str_dst, 0, buf_size);
-    unsigned char str_src_6[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9, 0x7e };
-    ret_val = botje_base64__encode(str_dst, str_src_6, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "FPucA9l+", 8);
+    {
+        // example from rfc4648
+        str_src_len = 6;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9, 0x7e };
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "FPucA9l+", num_chars);
+    }
 
     // clean up
     free(str_dst);
@@ -69,103 +83,112 @@ void test_botje_base64__encode_text_test(void **state) {
 
     // init and default values
     int64_t buf_size = 1024;
-    int64_t ret_val;
+    int64_t num_chars;
     int64_t str_src_len;
     unsigned char *str_dst = malloc(buf_size);
-    unsigned char *str_src = malloc(buf_size);
 
-    str_src_len = 1;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "M", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "TQ==", 4);
+    {
+        str_src_len = 1;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "M";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "TQ==", num_chars);
+    }
 
-    str_src_len = 2;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "Ma", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "TWE=", 4);
+    {
+        str_src_len = 2;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "Ma";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "TWE=", num_chars);
+    }
 
-    str_src_len = 3;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "Man", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 4);
-    assert_memory_equal(str_dst, "TWFu", 4);
+    {
+        str_src_len = 3;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "Man";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 4);
+        assert_memory_equal(str_dst, "TWFu", num_chars);
+    }
 
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "ManM", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "TWFuTQ==", 8);
+    {
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "ManM";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "TWFuTQ==", num_chars);
+    }
 
-    // example from rfc4648
-    str_src_len = 4;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "foob", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "Zm9vYg==", 8);
+    {
+        // example from rfc4648
+        str_src_len = 4;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "foob";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "Zm9vYg==", num_chars);
+    }
 
-    str_src_len = 5;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "ManMa", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "TWFuTWE=", 8);
+    {
+        str_src_len = 5;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "ManMa";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "TWFuTWE=", num_chars);
+    }
 
-    str_src_len = 5;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "ManXY", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "TWFuWFk=", 8);
+    {
+        str_src_len = 5;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "ManXY";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "TWFuWFk=", num_chars);
+    }
 
-    // example from rfc4648
-    str_src_len = 5;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "fooba", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "Zm9vYmE=", 8);
+    {
+        // example from rfc4648
+        str_src_len = 5;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "fooba";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "Zm9vYmE=", num_chars);
+    }
 
-    str_src_len = 6;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "ManMan", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "TWFuTWFu", 8);
+    {
+        str_src_len = 6;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "ManMan";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "TWFuTWFu", num_chars);
+    }
 
-    // example from rfc4648
-    str_src_len = 6;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "foobar", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 8);
-    assert_memory_equal(str_dst, "Zm9vYmFy", 8);
+    {
+        // example from rfc4648
+        str_src_len = 6;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "foobar";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 8);
+        assert_memory_equal(str_dst, "Zm9vYmFy", num_chars);
+    }
 
-    str_src_len = 27;
-    memset(str_dst, 0, buf_size);
-    memset(str_src, 0, buf_size);
-    memcpy(str_src, "Many hands make light work.", str_src_len);
-    ret_val = botje_base64__encode(str_dst, str_src, str_src_len);
-    assert_int_equal(ret_val, 36);
-    assert_memory_equal(str_dst, "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu", 36);
+    {
+        str_src_len = 27;
+        memset(str_dst, 0, buf_size);
+        unsigned char str_src[] = "Many hands make light work.";
+        num_chars = botje_base64__encode(str_dst, str_src, str_src_len);
+        assert_int_equal(num_chars, 36);
+        assert_memory_equal(str_dst, "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu", num_chars);
+    }
 
     // clean up
     free(str_dst);
-    free(str_src);
 }
